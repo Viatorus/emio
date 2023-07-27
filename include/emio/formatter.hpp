a26271bb99f5d2de0db04067b3b296d3dc91e7b0
Viatorus <lutztonineubert@gmail.com>
2023-07-27 21:27:54 +0200

    support ranges

--- include/emio/formatter.hpp
+++ include/emio/formatter.hpp
@@ -243,17 +243,37 @@ class formatter<detail::format_spec_with_value<T>> {
   }
 
   constexpr result<void> format(writer<char>& wtr, const detail::format_spec_with_value<T>& arg) noexcept {
-    // Change the spec if defined.
-    if (arg.spec.width != format_spec::not_defined) {
-      underlying_.set_width(arg.spec.width);
-    }
-    if (arg.spec.precision != format_spec::not_defined) {
-      underlying_.set_precision(arg.spec.precision);
-    }
+    overwrite_spec(arg.spec);
     return underlying_.format(wtr, arg.value);
   }
 
  private:
+  template <typename F>
+    requires requires(F x) {
+               x.set_width(1);
+               x.set_precision(1);
+             }
+  static constexpr F& get_core_formatter(F& formatter) noexcept {
+    return formatter;
+  }
+
+  template <typename F>
+    requires requires(F x) { x.underlying(); }
+  static constexpr auto& get_core_formatter(F& formatter) noexcept {
+    return get_core_formatter(formatter.underlying());
+  }
+
+  constexpr void overwrite_spec(const format_spec& spec) noexcept {
+    // Overwrite the spec of the core formatter if they are dynamically defined.
+    auto& f = get_core_formatter(underlying_);
+    if (spec.width != format_spec::not_defined) {
+      f.set_width(spec.width);
+    }
+    if (spec.precision != format_spec::not_defined) {
+      f.set_precision(spec.precision);
+    }
+  }
+
   formatter<T> underlying_{};
 };
 
