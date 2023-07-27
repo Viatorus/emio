9bb821e6578d87c008e7160a3028b8019cbed7b0
Viatorus <lutztonineubert@gmail.com>
2023-07-27 21:59:37 +0200

    static buffer

--- test/unit_test/test_buffer.cpp
+++ test/unit_test/test_buffer.cpp
@@ -204,6 +204,16 @@ TEST_CASE("buffer regression bug 1", "[buffer]") {
   CHECK(area->empty());
 }
 
+TEST_CASE("static_buffer", "[buffer]") {
+  // Test strategy:
+  // * Construct a static buffer.
+  // * Check size of the buffer.
+  // Expected: Every size is correct.
+
+  emio::static_buffer<char, 542> buffer;
+  CHECK(buffer.get_write_area_of(542));
+}
+
 TEST_CASE("basic_counting_buffer", "[buffer]") {
   // Test strategy:
   // * Construct a counting_buffer.
