fd24e4e033775fac9b62d29c89abbe60696b6d37
Viatorus <lutztonineubert@gmail.com>
2024-10-14 22:29:46 +0200

    not hosted, okay?

--- test/link_test/compile.cpp
+++ test/link_test/compile.cpp
@@ -11,8 +11,17 @@ void* _sbrk(int /*unused*/) {
 }
 }
 
+#if __STDC_HOSTED__
+#error shouldnt be hosted
+#endif
+
 int main() {
-  emio::static_buffer<32> buffer;
+//  emio::static_buffer<32> buffer{};
+
+  auto b = new emio::static_buffer<32>{};
+
+  emio::static_buffer<32> &buffer = *b;
+
   const auto result = emio::format_to(buffer, "Test: {:d}\n", 42);
   if (!result) {
     return -1;
