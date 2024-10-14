901044bc32bb25f557ae955731212abfa941155f
Viatorus <lutztonineubert@gmail.com>
2024-10-14 22:26:40 +0200

    come one

--- test/link_test/compile.cpp
+++ test/link_test/compile.cpp
@@ -1,5 +1,16 @@
 #include <emio/emio.hpp>
 
+extern "C" {
+void _exit(int /*unused*/) {
+  while (1) {
+  }
+}
+
+void* _sbrk(int /*unused*/) {
+  return 0;
+}
+}
+
 int main() {
   emio::static_buffer<32> buffer;
   const auto result = emio::format_to(buffer, "Test: {:d}\n", 42);
